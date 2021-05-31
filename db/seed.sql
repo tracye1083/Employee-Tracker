f47af4ca4632e1ccd8c341a9e99c8a2ff41185c7
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-05-31 17:54:36 -0400

    Update seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,36 @@
+USE company_db;
+
+INSERT INTO departments (`name`)
+VALUES ("Engineering");
+INSERT INTO departments (`name`)
+VALUES ("Finance");
+INSERT INTO departments (`name`)
+VALUES ("Legal");
+INSERT INTO departments (`name`)
+VALUES ("Sales");
+
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Lead Engineer", 150000, 1);
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Software Engineer", 120000, 1);
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Accountant", 125000, 2);
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Legal Team Lead", 250000, 3);
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Lawyer", 190000, 3);
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Sales Lead", 100000, 4);
+INSERT INTO roles (`title`, `salary`, `department_id`)
+VALUES ("Salesperson", 8000, 4);
+
+INSERT INTO employees (`first_name`, `last_name`, `role_id`, `manager_id`)
+VALUES ('Andy', 'Bernard', 2, 3);
+INSERT INTO employees (`first_name`, `last_name`, `role_id`, `manager_id`)
+VALUES ('Michael', 'Scott', 1, null);
+INSERT INTO employees (`first_name`, `last_name`, `role_id`, `manager_id`)
+VALUES ('Pam', 'Beesly', 3, 2);
+INSERT INTO employees (`first_name`, `last_name`, `role_id`, `manager_id`)
+VALUES ('Jim', 'Halpert', 4 ,3);
+INSERT INTO employees (`first_name`, `last_name`, `role_id`, `manager_id`)
+VALUES ('Dwight', 'Schrute', 5, 4);
