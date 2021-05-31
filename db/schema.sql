cb28ee1b70b8482c50c3fa95e0c31f2e4aef140d
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-05-31 18:20:06 -0400

    minor updates to syntax in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL (10,2),
-    department_id INT
+    salary DECIMAL (7,2),
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+    role_id INTEGER(8),
+    manager_id INTEGER(8),
+    PRIMARY KEY (id)
 );
