13e8dae84e852123bc7966f97ee778d2853062d4
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-05-31 23:23:02 -0400

    final updates
    testing program. need to look into manager bonus portion but that does not impact overall grade as it is not part of the required portion of the assignment. updates to colors and need to complete the assignment README and video to complete.

--- controllers/taskQuestion.js
+++ controllers/taskQuestion.js
@@ -4,27 +4,27 @@ const colors = require('colors');
 const questTask = [{
     type: 'list',
     name: 'task',
-    message: 'What would you like to do?'.cyan,
+    message: 'What would you like to do?'.brightCyan,
     choices: [
-        new inquirer.Separator('=== VIEW INFO ==='.rainbow),
+        new inquirer.Separator('=== VIEW INFO ==='.magenta),
         'View all employees',
         'View employees by manager',
         'View all roles',
         'View all departments',
-        new inquirer.Separator('=== ADD NEW INFO ==='.rainbow),
+        new inquirer.Separator('=== ADD NEW INFO ==='.magenta),
         'Add employee',
         'Add role',
         'Add department',
-        new inquirer.Separator('=== UPDATE INFO ==='.rainbow),
+        new inquirer.Separator('=== UPDATE INFO ==='.magenta),
         'Update employee',
-        new inquirer.Separator('=== DELETE INFO ==='.rainbow),
+        new inquirer.Separator('=== DELETE INFO ==='.magenta),
         'Delete employee',
         'Delete role',
         'Delete department',
-        new inquirer.Separator('=== EXIT ==='.rainbow),
-        'Exit Application'.yellow,
+        new inquirer.Separator('=== EXIT ==='.magenta),
+        'Exit Application'.brightMagenta,
     ],
-    pageSize: 17
+    pageSize: 18
 }];
 
 module.exports = questTask;
